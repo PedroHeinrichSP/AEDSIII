1c91b2c3280659df2f13899128bb9fd24c9f309f
HeinrichFeather <103895487+PedroHeinrichSP@users.noreply.github.com>
2023-02-12 13:03:55 -0300

    Atualização do Projeto
    Mudança de csv, adição do opencsv, mudança de classes

--- CODIGO/TP01.java
+++ CODIGO/TP01.java
@@ -1,241 +1,213 @@
+import java.util.Calendar;
 import java.util.Date;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 class TP01{
-    public static class Movie{
-        //Atributos
-        String title; // Título do filme   
-        Date year; // Data padrão, definir e usar somente o ano
-        String certificate; // Unrated, (Banned), U, UA, G, A, PG-13, 12+, 15+, 16+, 18+, 7+, All, Approved, GP, M/PG, NC-17, Not Rated, PG, R, 7, 12, 13, 16, 18, U/A, UA 13+, UA 16+, UA 7+
-        int runtime; // Inteiro em minutos
-        String[] genre; // Multiplas linhas para gênero
-        float rating; // Média das notas de 0 a 10 do público (até uma casa decimal)
-        int metascore; // Média das notas de 0 a 100 de críticos
-        String synopsis; // Avaliar o uso
-        String director; // Nome do diretor
-        int votes; // Número de votos que o filme teve
-        float gross; // Renda do filme (em milhões, duas casas decimais)
-        String[] cast; // Nome dos principais atores (até 4 pessoas)
+    public class Pokedex{
+        /*
+         * N° (ID - número da pokedex) - tipo Inteiro (short)
+         * Name (Nome - Nome do Pokemon, formas diferentes incluídas) - tipo Fixo (String)
+         * Generation (Geração - Data de lançamento da geração) - tipo Data (Date)
+         * Height (Altura - Altura do pokemon em metros) - tipo Float (Float)
+         * Weight (Peso - Peso do pokemon em kilogramas) - tipo Float (Float)
+         * Type1 & Type2 (Tipos - Tipagem do pokemon) - tipo Variavel (String[])
+         * Category (Categoria - Categoria do pokemon) - tipo Valores (byte)
+         * Mega_Evolution_Flag (Pode MegaEvoluir - O pokemon tem mega evolução) - tipo Valores (boolean)
+         * TOTAL (Status - Total da soma de status do pokemon) - tipo Valores (int)
+         */
+        private short ID; 
+        private String Name;
+        private Date Generation;
+        private float Height;
+        private float Weight;
+        private String[] Type;
+        private byte Category;
+        private boolean Mega_Evolution_Flag;
+        private int TOTAL;
 
-        //Construtores
-
-        public Movie(){
-            this.title = "";
-            this.year = new Date();
-            this.certificate = "";
-            this.runtime = 0;
-            this.genre = new String[0];
-            this.rating = 0;
-            this.metascore = 0;
-            this.synopsis = "";
-            this.director = "";
-            this.votes = 0;
-            this.gross = 0;
-            this.cast = new String[0];
-        }
-
-        public Movie(String title, Date year, String certificate, int runtime, String[] genre, float rating, int metascore, String synopsis, String director, int votes, float gross, String[] cast){
-            this.title = title;
-            this.year = year;
-            this.certificate = certificate;
-            this.runtime = runtime;
-            this.genre = genre;
-            this.rating = rating;
-            this.metascore = metascore;
-            this.synopsis = synopsis;
-            this.director = director;
-            this.votes = votes;
-            this.gross = gross;
-            this.cast = cast;
+        // Construtor
+        public Pokedex(short ID, String Name, Date Generation, float Height, float Weight, String[] Type, byte Category, boolean Mega_Evolution_Flag, int TOTAL){
+            this.ID = ID;
+            this.Name = Name;
+            this.Generation = Generation;
+            this.Height = Height;
+            this.Weight = Weight;
+            this.Type = Type;
+            this.Category = Category;
+            this.Mega_Evolution_Flag = Mega_Evolution_Flag;
+            this.TOTAL = TOTAL;
         }
 
-
-
-        //Getters
-        public String getTitle(){
-            return this.title;
+        // Getters
+        public short getID(){
+            return this.ID;
         }
-        public Date getYear(){
-            return this.year;
+        public String getName(){
+            return this.Name;
         }
-        public String getCertificate(){
-            return this.certificate;
+        public Date getGeneration(){
+            return this.Generation;
         }
-        public int getRuntime(){
-            return this.runtime;
+        public float getHeight(){
+            return this.Height;
         }
-        public String[] getGenre(){
-            return this.genre;
+        public float getWeight(){
+            return this.Weight;
         }
-        public float getRating(){
-            return this.rating;
+        public String[] getType(){
+            return this.Type;
         }
-        public int getMetascore(){
-            return this.metascore;
+        public byte getCategory(){
+            return this.Category;
         }
-        public String getSynopsis(){
-            return this.synopsis;
+        public boolean getMega_Evolution_Flag(){
+            return this.Mega_Evolution_Flag;
         }
-        public String getDirector(){
-            return this.director;
-        }
-        public int getVotes(){
-            return this.votes;
-        }
-        public float getGross(){
-            return this.gross;
-        }
-        public String[] getCast(){
-            return this.cast;
-        }
-
-        //Setters
-        public void setTitle(String title){
-            this.title = title;
-        }
-        public void setYear(Date year){
-            this.year = year;
-        }
-        public void setCertificate(String certificate){
-            this.certificate = certificate;
-        }
-        public void setRuntime(int runtime){
-            this.runtime = runtime;
-        }
-        public void setGenre(String[] genre){
-            this.genre = genre;
-        }
-        public void setRating(float rating){
-            this.rating = rating;
-        }
-        public void setMetascore(int metascore){
-            this.metascore = metascore;
-        }
-        public void setSynopsis(String synopsis){
-            this.synopsis = synopsis;
-        }
-        public void setDirector(String director){
-            this.director = director;
-        }
-        public void setVotes(int votes){
-            this.votes = votes;
-        }
-        public void setGross(float gross){
-            this.gross = gross;
-        }
-        public void setCast(String[] cast){
-            this.cast = cast;
-        }
-
-        //Métodos
-        public void print(){
-            System.out.println("Título: " + this.title);
-            System.out.println("Ano: " + this.year);
-            System.out.println("Certificado: " + this.certificate);
-            System.out.println("Duração: " + this.runtime);
-            System.out.println("Gênero: " + this.genre);
-            System.out.println("Nota: " + this.rating);
-            System.out.println("Metascore: " + this.metascore);
-            System.out.println("Sinopse: " + this.synopsis);
-            System.out.println("Diretor: " + this.director);
-            System.out.println("Votos: " + this.votes);
-            System.out.println("Renda: " + this.gross);
-            System.out.println("Elenco: " + this.cast);
-        }
-
-        public Movie clone(){
-            Movie movie;
-            movie = new Movie(this.title, this.year, this.certificate, this.runtime, this.genre, this.rating, this.metascore, this.synopsis, this.director, this.votes, this.gross, this.cast);
-            return movie;
+        public int getTOTAL(){
+            return this.TOTAL;
         }
         
-        public boolean equals(Movie movie){
-            boolean equals = false;
-            if(this.title.equals(movie.title) && this.year.equals(movie.year) && this.certificate.equals(movie.certificate) && this.runtime == movie.runtime && this.genre.equals(movie.genre) && this.rating == movie.rating && this.metascore == movie.metascore && this.synopsis.equals(movie.synopsis) && this.director.equals(movie.director) && this.votes == movie.votes && this.gross == movie.gross && this.cast.equals(movie.cast)){
-                equals=true;
-            } else equals=false;
-            return equals;
+        // Setters
+        public void setID(short ID){
+            this.ID = ID;
+        }
+        public void setName(String Name){
+            this.Name = Name;
+        }
+        public void setGeneration(Date Generation){
+            this.Generation = Generation;
+        }
+        public void setHeight(float Height){
+            this.Height = Height;
+        }
+        public void setWeight(float Weight){
+            this.Weight = Weight;
+        }
+        public void setType(String[] Type){
+            this.Type = Type;
+        }
+        public void setCategory(byte Category){
+            this.Category = Category;
+        }
+        public void setMega_Evolution_Flag(boolean Mega_Evolution_Flag){
+            this.Mega_Evolution_Flag = Mega_Evolution_Flag;
+        }
+        public void setTOTAL(int TOTAL){
+            this.TOTAL = TOTAL;
         }
 
+        // toString
         public String toString(){
-            return "Título: " + this.title + "\nAno: " + this.year + "\nCertificado: " + this.certificate + "\nDuração: " + this.runtime + "\nGênero: " + this.genre + "\nNota: " + this.rating + "\nMetascore: " + this.metascore + "\nSinopse: " + this.synopsis + "\nDiretor: " + this.director + "\nVotos: " + this.votes + "\nRenda: " + this.gross + "\nElenco: " + this.cast;
+            return "ID: " + this.ID + " | Name: " + this.Name + " | Generation: " + this.Generation + " | Height: " + this.Height + " | Weight: " + this.Weight + " | Type: " + this.Type + " | Category: " + this.Category + " | Mega_Evolution_Flag: " + this.Mega_Evolution_Flag + " | TOTAL: " + this.TOTAL;
         }
 
-        public int compareToYear(Movie movie){
-            int compare = 0;
-            if(this.year.compareTo(movie.year) > 0){
-                compare = 1;
-            } else if(this.year.compareTo(movie.year) < 0){
-                compare = -1;
-            } else if(this.year.compareTo(movie.year) == 0){
-                compare = 0;
+        // Clone
+        public Pokedex clone(){
+            Pokedex clone = new Pokedex(this.ID, this.Name, this.Generation, this.Height, this.Weight, this.Type, this.Category, this.Mega_Evolution_Flag, this.TOTAL);
+            return clone;
+        }
+
+        // equals
+        public boolean equals(Object obj){
+            if(obj == null){
+                return false;
             }
-            return compare;
-        }
-
-        public int compareToRuntime(Movie movie){
-            int compare = 0;
-            if(this.runtime > movie.runtime){
-                compare = 1;
-            } else if(this.runtime < movie.runtime){
-                compare = -1;
-            } else if(this.runtime == movie.runtime){
-                compare = 0;
+            if(obj == this){
+                return true;
             }
-            return compare;
-        }
-
-        public int compareToRating(Movie movie){
-            int compare = 0;
-            if(this.rating > movie.rating){
-                compare = 1;
-            } else if(this.rating < movie.rating){
-                compare = -1;
-            } else if(this.rating == movie.rating){
-                compare = 0;
+            if(obj.getClass() != this.getClass()){
+                return false;
             }
-            return compare;
+            Pokedex p = (Pokedex)obj;
+            return (this.ID == p.ID && this.Name.equals(p.Name) && this.Generation.equals(p.Generation) && this.Height == p.Height && this.Weight == p.Weight && this.Type.equals(p.Type) && this.Category == p.Category && this.Mega_Evolution_Flag == p.Mega_Evolution_Flag && this.TOTAL == p.TOTAL);
         }
 
-        public int compareToMetascore(Movie movie){
-            int compare = 0;
-            if(this.metascore > movie.metascore){
-                compare = 1;
-            } else if(this.metascore < movie.metascore){
-                compare = -1;
-            } else if(this.metascore == movie.metascore){
-                compare = 0;
+        // compareTo
+        public int compareTo(Pokedex p){
+            if(this.ID > p.ID){
+                return 1;
+            }else if(this.ID < p.ID){
+                return -1;
+            }else{
+                return 0;
             }
-            return compare;
         }
 
-        public int compareToVotes(Movie movie){
-            int compare = 0;
-            if(this.votes > movie.votes){
-                compare = 1;
-            } else if(this.votes < movie.votes){
-                compare = -1;
-            } else if(this.votes == movie.votes){
-                compare = 0;
-            }
-            return compare;
+        // isMega
+        public boolean isMega(){
+            return this.Mega_Evolution_Flag;
         }
 
-        public int compareToGross(Movie movie){
-            int compare = 0;
-            if(this.gross > movie.gross){
-                compare = 1;
-            } else if(this.gross < movie.gross){
-                compare = -1;
-            } else if(this.gross == movie.gross){
-                compare = 0;
-            }
-            return compare;
+        // isOrdinary
+        public boolean isOrdinary(){
+            return (this.Category == 1);
         }
 
-        
+        // isLegendary
+        public boolean isLegendary(){
+            return (this.Category == 2);
+        }
 
+        // isMythical
+        public boolean isMythical(){
+            return (this.Category == 3);
+        }
     }
-    public static void main(String[] args){
-        
+
+    public class Gen{
+        final Date gen1 = setTime(1996, 1, 27).getTime();
+        final Date gen2 = setTime(1999, 10, 21).getTime();
+        final Date gen3 = setTime(2002, 10, 21).getTime();
+        final Date gen4 = setTime(2006, 8, 28).getTime();
+        final Date gen5 = setTime(2010, 8, 18).getTime();
+        final Date gen6 = setTime(2013, 11, 12).getTime();
+        final Date gen7 = setTime(2016, 10, 18).getTime();
+        final Date gen8 = setTime(2019, 10, 15).getTime();
+        final Date gen9 = setTime(2022, 10, 18).getTime();
+        // Base de dados para conversão, geração (int) para tipo Date (data de lançamento da gen), no parsing do CSV
+        public Calendar setTime (int year, int month, int day){
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.YEAR, year);
+            cal.set(Calendar.MONTH, month);
+            cal.set(Calendar.DAY_OF_MONTH, day);
+            return cal;
+        }
+    }
+
+    public static class Parser{
+        // Construtor   
+        public Parser(File file){
+            try {
+                teste(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void teste(File file) throws IOException{
+            CSVReader reader = new CSVReader(new FileReader(file));
+            String[] line;
+            try {
+                while((line = reader.readNext()) != null){
+                    for (String column : line) {
+                        System.out.print(column + " ");
+                    }
+                }
+            } catch (CsvValidationException e) {
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        // File
+        File file = new File("D:\\AEDSIII\\CSV\\Pokedex.csv");
+        // Parser
+        Parser p = new Parser(file);
     }
 }
